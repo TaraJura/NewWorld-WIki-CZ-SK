2fed601277aa957b20ddb1aa8a228f8020897312
zeddosx <54745487+zeddosx@users.noreply.github.com>
2021-09-22 20:32:49 +0200

    update

--- src/Itemy.js
+++ src/Itemy.js
@@ -3,8 +3,13 @@ import './itemy.css';
 
 export default function Itemy() {
     return (
-        <div id="itemy" >
-            <iframe src="https://nwdb.info" width="99.%" height="820px" id="iframeItemy"></iframe>
-        </div>
+            <div id="itemy" >
+                <script>
+                    window.onload = function() {
+                    document.getElementsByClassName("btn-primary")
+                };
+                </script>
+                <iframe src="https://nwdb.info" width="99%" height="820px" id="iframeItemy"></iframe>
+            </div>
     )
 }
