0a1084b9470f8c8a44058c44bf20b8e53e6ded1e
Jiří Novák <jirkanovakneda11@seznam.cz>
2021-09-22 23:29:28 +0200

    update

--- src/Itemy.js
+++ src/Itemy.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './itemy.css';
+import Iframescript from './scripts/iframescript';
 
 export default function Itemy() {
     return (
             <div id="itemy" >
-                <iframe src="https://nwdb.info" width="99%" height="820px" id="iframeItemy"></iframe>
-                <script>
-           
-                </script>
+                <iframe src="https://nwdb.info" width="99%" height="820px" id="iframeItemy" title="itemy"></iframe>
+                    <button onClick={<Iframescript />}></button>
             </div>
     )
 }
+
+
+/** SCRIPT : document.querySelector("#svelte > div.container.mt-4.container-mb.my-auto.svelte-tg35q7 > div.container.main-page > div > div.col-lg-7.p-3.p-lg-1.p-xl-5.pt-lg-3 > div > a.btn.btn-primary.btn-lg.px-4\!.fw-bold").click(); */
