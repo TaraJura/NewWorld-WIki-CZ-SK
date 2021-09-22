c8cd0bdf5f74627c60a06f47aabd780641e1a9b0
Jiří Novák <jirkanovakneda11@seznam.cz>
2021-09-22 22:36:08 +0200

    update Home folderu

--- src/Itemy.js
+++ src/Itemy.js
@@ -3,8 +3,11 @@ import './itemy.css';
 
 export default function Itemy() {
     return (
-        <div id="itemy" >
-            <iframe src="https://nwdb.info" width="99.%" height="820px" id="iframeItemy"></iframe>
-        </div>
+            <div id="itemy" >
+                <iframe src="https://nwdb.info" width="99%" height="820px" id="iframeItemy"></iframe>
+                <script>
+           
+                </script>
+            </div>
     )
 }
