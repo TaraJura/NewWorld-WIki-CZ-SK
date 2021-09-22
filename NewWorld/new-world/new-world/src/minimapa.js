e822ffc8ce2ca029a2c09fae0da37a25262bdc6f
zeddosx <54745487+zeddosx@users.noreply.github.com>
2021-09-22 19:35:08 +0200

    asasddas

---
+++ NewWorld/new-world/new-world/src/minimapa.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './minimapa.css';
+
+export default function Minimapa() {
+    return (
+        <div id="minimapa" >
+            <iframe src="https://mapgenie.io/new-world/maps/aeternum" width="99.%" height="820px" id="iframeminimapa"></iframe>
+        </div>
+    )
+}
